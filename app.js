850a1972907b183eaef2430bb3139b50ad70157c
paresh2421 <paresh2411@gmail.com>
2025-04-22 15:41:28 +0530

    Third Commit

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import dotenv from 'dotenv'
 const app = express()
 
 import connect from './db/connect.js'
-import router from './routes/urls.js'
+import urlRouter from './routes/urls.js'
+
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('<h1> URL Shortener </h1>')
-})
+// app.get('/', (req, res) => {
+//   res.send('<h1> URL Shortener </h1>')
+// })
 
-app.use('/api/v1/shorten', router)
+app.use('/api/v1', urlRouter);
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 3000
 
 const start = async () => {
